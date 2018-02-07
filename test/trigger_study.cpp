87d4c03d29d805ff115e4652ea8b827aa72732a9
Daniel Fernando Guerrero Ibarra <daguerre@lxplus078.cern.ch>
2018-02-07 19:34:37 +0100

    Update Feb 7

--- test/trigger_study.cpp
+++ test/trigger_study.cpp
@@ -20,7 +20,6 @@ namespace oph = OfflineProducerHelper;
 #include "TFile.h"
 
 using namespace std;
-
 // trigger_study.exe --cfg config/skim.cfg --input inputFiles/Samples_80X/VBF_HH_4b_10gen2018_test.txt --maxEvts 1000
 
 int main(int argc, char** argv)
@@ -84,15 +83,60 @@ int main(int argc, char** argv)
     cout << "[INFO] ... file list contains " << nfiles << " files" << endl;
     cout << "[INFO] ... creating tree reader" << endl;
     NanoAODTree nat (&ch);
-    cout << "[INFO] ... loading the following triggers" << endl;
-    vector <string> trigger_path;
 
+    cout << "[INFO] ... reading all HLT paths" << endl;
+    vector <string> trigger_paths;
+    TObjArray* List = ch.GetListOfLeaves();
+    for(int  j=0;  j < List->GetEntries();  j++){
+              string name = List->At(j)->GetName();
+              if(name.substr(0,4)=="HLT_"){
+              trigger_paths.push_back(name);
+              }
+    }    
+
+    cout << "[INFO] ... getting the HLT efficiencies" << endl;
+    vector <int> ntrig(trigger_paths.size(),0);    
+    int ntot=0;
+    int maxEvts = opts["maxEvts"].as<int>();
+    nat.triggerReader().setTriggers(trigger_paths);  
+  
+    ////////////////////////////////////////////////////////////////////////
+    // Execute Event loop
+    ////////////////////////////////////////////////////////////////////////        
+    for (int iEv = 0; true; ++iEv)
+        {
+        if (maxEvts >= 0 && iEv >= maxEvts)break;
+        if (!nat.Next()) break;
+        if (iEv % 10000 == 0) cout << "... processing event " << iEv << endl;
+        ////////////////////////////////////////////////////////////////////////
+        // Execute HLT paths loop
+        ////////////////////////////////////////////////////////////////////////  
+        for (unsigned k=0; k<trigger_paths.size(); k++){        
+          if (nat.triggerReader().getTrgResult(trigger_paths[k])) ntrig[k]=ntrig[k]+1;
+        }
+        ++ntot;
+    }
+
+    cout << "[INFO] ... saving the HLT efficiencies" << endl;
+    ofstream outputFile;
+    outputFile.open("efficiencies.txt");
+    for (unsigned p=0; p<trigger_paths.size(); p++){        
+           outputFile << setw(100)<< trigger_paths[p] << setw(20)<<(double)ntrig[p]/ntot<<endl;
+    }
+    outputFile.close();
+
+
+
+
+/*
+    cout << "[INFO] ... loading the best triggers" << endl;
+    vector <string> trigger_path;
     for (auto trg : config.readStringListOpt("triggers::makeORof")){
         cout << "   - " << trg << endl;
         trigger_path.push_back(trg); 
     }
-
     nat.triggerReader().setTriggers(config.readStringListOpt("triggers::makeORof"));
+
 
     ////////////////////////////////////////////////////////////////////////
     // Execute event loop
@@ -100,7 +144,7 @@ int main(int argc, char** argv)
     int ntot = 0;
     int trig1 = 0;   
     int trig2 = 0;
-    int trigOR = 0;  
+    int trigOR = 0;   
     int maxEvts = opts["maxEvts"].as<int>();
     if (maxEvts >= 0)
         cout << "[INFO] ... running on : " << maxEvts << " events" << endl;
@@ -113,7 +157,8 @@ int main(int argc, char** argv)
         if (!nat.Next()) break;
         if (iEv % 10000 == 0) cout << "... processing event " << iEv << endl;
 
-        ++ntot;    
+        ++ntot;
+
         ////////////////////////////////////////////////////////////////////////
         // Check individual and OR trigger status
         ////////////////////////////////////////////////////////////////////////
@@ -127,4 +172,8 @@ int main(int argc, char** argv)
     cout << "|Path       |"<< setw(30) << trigger_path[0] << setw(30) << trigger_path[1] << setw(30) <<"OR"<< endl;
     cout << "|Events     |"<< setw(30) << trig1 << setw(30) << trig2 << setw(30) << trigOR << endl;
     cout << "|Efficiency |"<< setw(30) << (double)trig1 / ntot << setw(30) << (double)trig2 / ntot << setw(30) << (double)trigOR / ntot << endl;
+
+
+*/
 }
+
